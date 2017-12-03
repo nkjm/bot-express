7b21a88deecfdedd8cd6ad5fb4debc026dc855a0
Nakajima Kazuki <JP22274@w022571708172m.local>
2017-12-03 22:20:33 +0900

    improving

--- module/memory/mongodb.js
+++ module/memory/mongodb.js
@@ -8,55 +8,40 @@ Promise = require("bluebird");
 class MemoryMongodb {
     constructor(options){
         this.url = options.url;
+        this.connected = MongoClient.connect(this.url, {promiseLibrary: Promise}).then((response) => {
+            this.db = response;
+        });
     }
 
     get(key){
-        return new Promise((resolve, reject) => {
-            return MongoClient.connect(this.url, function(err, db) {
-                assert.equal(null, err);
-                debug("Connected successfully to MongoDB server");
-
-                return db.collection('bot-express').findOne({context_id: key}).then((response) => {
-                    debug(response);
-                    return resolve(response);
-                }).catch((error) => {
-                    return reject(error);
-                });
+        return this.connected.then((response) => {
+            return db.collection('bot-express').findOne({context_id: key}).then((response) => {
+                return resolve(response);
+            }).catch((error) => {
+                return reject(error);
             });
         });
     }
 
     put(key, value, retention){
-        return new Promise((resolve, reject) => {
-            return MongoClient.connect(this.url, function(err, db) {
-                assert.equal(null, err);
-                debug("Connected successfully to MongoDB server");
-
-                value.context_id = key;
-                return db.collection('bot-express').insertOne(value).then((response) => {
-                    assert.equal(1, response.insertedCount);
-                    return resolve();
-                }).catch((error) => {
-                    return reject(error);
-                });
+        return this.connected.then((response) => {
+            value.context_id = key;
+            return db.collection('bot-express').insertOne(value).then((response) => {
+                assert.equal(1, response.insertedCount);
+                return resolve();
+            }).catch((error) => {
+                return reject(error);
             });
         });
     }
 
     del(key){
-        return new Promise((resolve, reject) => {
-            return MongoClient.connect(this.url, function(err, db) {
+        return this.connected.then((response) => {
+            return db.collection('bot-express').deleteOne({context_id: key}).then((response) => {
                 assert.equal(null, err);
-                debug("Connected successfully to MongoDB server");
-
-                return db.collection('bot-express').deleteOne({context_id: key}, function(err, r) {
-                    try {
-                        assert.equal(null, err);
-                        return resolve();
-                    } catch(e) {
-                        return reject(e);
-                    }
-                });
+                return resolve();
+            }).catch((error) => {
+                return reject(error);
             });
         });
     }

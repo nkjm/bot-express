639c7ca1a7e351445060bdee87aa8087953a442b
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-09-28 09:34:34 +0900

    Fix bug in doc

--- typedef/global.js
+++ typedef/global.js
@@ -1,7 +1,7 @@
 /**
 Object which contains context information.
 @typedef {Object} context
-@prop {String} to_confirm - Array of parameter names to confirm.
+@prop {Array.<Object>} to_confirm - Array of parameter names to confirm.
 @prop {Sting} confirming - Parameter name which Bot is now confirming.
 @prop {Object} confirmed - Object which contains confirmed value of the parameters as properties. If you want to retrieve confirmed value of "date" parameter, access confirmed.date.
 @prop {Object} previous - Object which contains conversation history in the current context.

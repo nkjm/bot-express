52b197520f0a1b95d43141bec54c84f3ddd8f523
Kazuki Nakajima <nakajima@bot-express.com>
2021-03-11 09:58:42 +0900

    Add TranslaterSalesforce

---
+++ module/translator/salesforce.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const debug = require("debug")("bot-express:translator");
+const required_option_list = ["db"]
+
+/**
+* Translator implementation of salesforce
+* @class
+*/
+class TranslatorSalesforce {
+    /**
+     * @constructor
+     * @param {Object} options
+     * @param {Object} options.db
+    */
+    constructor(options){
+        for (let required_option of required_option_list){
+            if (!options[required_option]){
+                throw new Error(`Required option "${required_option}" of TranslatorSalesforce not set.`);
+            }
+        }
+
+        this.db = options.db
+        this.translator = this.db
+    }
+
+    async detect(text){
+        debug(`detect method is not supported in TranslatorSalesforce.`)
+    }
+
+    async translate(text, lang){
+        return db.get_localization({
+            label: text,
+            lang,
+        })
+    }
+}
+
+module.exports = TranslatorSalesforce;

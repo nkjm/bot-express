b0b8d86f79f0cc912050483a3f8dbef8d7f2d390
akira108 <akira.iwaya@bot-express.com>
2024-04-18 17:02:38 +0900

    Load default exported module from compiled code from typescript

--- src/module/bot.js
+++ src/module/bot.js
@@ -2,7 +2,8 @@
 
 const debug = require("debug")("bot-express:bot");
 const Translator = require("./translator");
-const clone = require("rfdc/default")
+const clone = require("rfdc/default");
+const { getDefault } = require("./utils");
 
 /**
  * Toolkit to be used by skill.
@@ -397,7 +398,7 @@ class Bot {
         if (!(param.generator.file && param.generator.method)) {
             throw Error(`Generator of ${param_name} is not correctly set.`);
         }
-        const Generator = require(`${this._options.parameter_path}${param.generator.file}`);
+        const Generator = getDefault(require(`${this._options.parameter_path}${param.generator.file}`));
         const generator = new Generator();
         if (!generator[param.generator.method])
             throw Error(`${param.generator.file} does not have ${param.generator.method}`);
